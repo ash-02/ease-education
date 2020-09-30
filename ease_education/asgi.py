346a65b404b916f4fda0018b27d1a4f3c91a100a
suriyakrishnan <suriya@outlook.in>
2020-09-30 17:59:05 +0530

    working state:1 - Django

--- ease_education/asgi.py
+++ ease_education/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for easeeducation project.
+ASGI config for ease_education project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easeeducation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ease_education.settings')
 
 application = get_asgi_application()
--- ease_education/wsgi.py
+++ ease_education/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for easeeducation project.
+WSGI config for ease_education project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easeeducation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ease_education.settings')
 
 application = get_wsgi_application()
